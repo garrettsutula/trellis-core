8fe1154b00aa9e6442f64b60be45264f34d0ffab
Garrett Sutula <garrettsutula@gmail.com>
2022-09-26 11:11:02 -0400

    Register new helpers

--- src/templates/helpers/index.ts
+++ src/templates/helpers/index.ts
@@ -1,5 +1,7 @@
 import ifCond from './ifCond';
 import includes from './includes';
 import repeat from './repeat';
+import _switch from './switch';
+import _case from './case';
 
-export default [ ifCond, includes, repeat ];
+export default [ ifCond, includes, repeat, _switch, _case ];
